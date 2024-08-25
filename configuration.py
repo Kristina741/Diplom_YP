bde2a01a3cb87aa053b915c8ac9b569ac38f6a73
kkist <kkist@DESKTOP-D2A3J6U.(none)>
2024-08-25 16:55:32 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://df9f0420-c1e6-4ca3-879f-377f1a071ef3.serverhub.praktikum-services.ru" # адрес сервера
+CREATE_COURIER = "/api/v1/courier"   # создание курьера
+CREATE_ORDER = "/api/v1/orders"  # создание заказа
+ORDER_INFORMATION = "/api/v1/orders/track?t="   # получить информацию о заказе по его номеру
